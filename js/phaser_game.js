82845b2a006a39e981db6a9933a7703beb25e173
Anthony Castelli <anthonycastelli01@gmail.com>
2015-09-24 18:33:29 -0700

    Add gems

--- js/phaser_game.js
+++ js/phaser_game.js
@@ -1,10 +1,11 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
-var player, platforms, cursors;
+var player, platforms, cursors, keys;
 
 function preload() {
   game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+  game.load.image('gem', 'assets/diamond.png');
 }
 
 function create() {
@@ -32,12 +33,19 @@ function create() {
   player.animations.add('left', [0, 1, 2, 3], 10, true);
   player.animations.add('right', [5, 6, 7, 8], 10, true);
 
+  // Gems setup
+  gems = game.add.group();
+  game.physics.arcade.enable(gems);
+  gems.enableBody = true;
+
   // Controls
   cursors = game.input.keyboard.createCursorKeys();
+  keys = game.input.keyboard.addKeys({'space': Phaser.Keyboard.SPACEBAR})
 }
 
 function update() {
   game.physics.arcade.collide(player, platforms);
+  game.physics.arcade.collide(gems, platforms);
 
   if (player.body.touching.down) {
     player.body.velocity.x = 0;
@@ -58,5 +66,17 @@ function update() {
   // Jumping
   if (cursors.up.isDown && player.body.touching.down) {
     player.body.velocity.y = -350;
+  }
+
+  // Make gems
+  if (keys.space.isDown) {
+    console.log("Space pressed!");
+
+    var gem = gems.create(
+                      Math.floor(Math.random() * 500) + 1,
+                      Math.floor(Math.random() * 20) + 1,
+                      'gem');
+    gem.body.bounce.y = 0.5 + Math.random() * 0.2;;
+    gem.body.gravity.y = 300;
   }
 }
